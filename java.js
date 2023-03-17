a5b54d4d4dfefe51f36b4c19eed49686ce09beda
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2023-03-17 00:18:52 -0400

    Deletes optional text on share
    Messes with copying link on IOS so better off w/o it

--- java.js
+++ java.js
@@ -1,7 +1,6 @@
 const shareLink = document.querySelector('#share-link')
 const link = {
   title: "WebDevJev linktree",
-  text: "Thank you for sharing my links!",
   url: 'https://jevonthompsonx.github.io/LinkTree-Clone/',
 };
 
